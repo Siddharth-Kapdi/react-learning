e053a4d06975ed4501155914d78d726b88d7090e
Siddharth Kapdi <siddharth.developerr@gmail.com>
2026-08-05 14:21:17 +0530

    add useEffect warm-up(practice) lab

---
+++ Hooks/useEffect/warmup-lab/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import UseEffectPractice from './UseEffectPractice'
+
+function App() {
+  return (
+    <div className="container">
+      <UseEffectPractice />
+    </div>
+  )
+}
+
+export default App
---
+++ Hooks/useEffect/warmup-lab/src/UseEffectPractice.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react'
+
+const UseEffectPractice = () => {
+  const [count, setCount] = useState(0)
+  const [name, setName] = useState('')
+
+  useEffect(() => {
+    document.title = `count: ${count}`
+    console.log(`Name: ${name}`)
+  }, [count, name])
+  return (
+    <div className="child-container">
+      <h1>useEffect Practice</h1>
+      <div className="counter-container">
+        <p className="count">{count}</p>
+        <button className="count-btn" onClick={() => setCount(count + 1)}>
+          Increment
+        </button>
+      </div>
+      <div className="name-container">
+        <p className="name">Name: {name}</p>
+        <input
+          type="text"
+          name="name"
+          id="name-input"
+          placeholder="Enter your name..."
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default UseEffectPractice
